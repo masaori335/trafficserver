2be62313d3c14d3772b0334a88dce3ad4aa5494b
Masaori Koshiba <masaori@apache.org>
2022-11-08 10:36:32 +0900

    Cleanup: Remove INK_QUEUE_NT

--- include/tscore/ink_hrtime.h
+++ include/tscore/ink_hrtime.h
@@ -219,11 +219,6 @@ ink_hrtime_to_timeval(ink_hrtime t)
   return (tv);
 }
 
-/*
-   using Jan 1 1970 as the base year, instead of Jan 1 1601,
-   which translates to (365 + 0.25)369*24*60*60 seconds   */
-#define NT_TIMEBASE_DIFFERENCE_100NSECS 116444736000000000i64
-
 static inline ink_hrtime
 ink_get_hrtime_internal()
 {
--- include/tscore/ink_queue.h
+++ include/tscore/ink_queue.h
@@ -223,12 +223,7 @@ struct InkAtomicList {
   uint32_t offset  = 0;
 };
 
-#if !defined(INK_QUEUE_NT)
-#define INK_ATOMICLIST_EMPTY(_x) (!(TO_PTR(FREELIST_POINTER((_x.head)))))
-#else
-/* ink_queue_nt.c doesn't do the FROM/TO pointer swizzling */
 #define INK_ATOMICLIST_EMPTY(_x) (!((FREELIST_POINTER((_x.head)))))
-#endif
 
 // WARNING: the "name" string is not copied, it has to be a statically-stored constant string.
 //
